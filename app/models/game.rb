2ee18b69047af99e8d585c7d5905af75696520c5
Arit Amana <>
2018-05-16 10:33:24 -0400

    Fix database and RSpec issues

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,8 @@ class Game < ApplicationRecord
   
   has_many :pieces
 
-  belongs_to :white_player, :class_name => 'User'
-  belongs_to :black_player, :class_name => 'User'
+  belongs_to :white_player, class_name: 'User'
+  belongs_to :black_player, class_name: 'User', required: false
 
-  validates :name, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :game_name, presence: true, length: { maximum: 140, minimum: 3 }
 end
