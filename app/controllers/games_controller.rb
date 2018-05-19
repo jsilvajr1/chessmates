f766dc64de5d1df6bf339fe45d1a449b0869c644
Bryan Jimenez <bryan@roguedevs.io>
2018-05-19 00:58:52 +0000

    worked on joining available game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,6 +24,18 @@ class GamesController < ApplicationController
     return render_not_found if @game.blank?
   end
 
+  def update
+    @game = Game.find(params[:id])
+    @game.update_attributes(black_player_id: current_user.id)
+
+    if @game.black_player_joined?
+      flash[:notice] = 'You joined the game succesfully'
+      redirect_to game_path(@game)
+    else
+      flash[:alert] = 'You did not join the game. Please try again'
+    end
+  end
+
   def destroy
     @game = Game.find_by_id(params[:id])
     return render_not_found if @game.blank?
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,14 @@
 class Game < ApplicationRecord
-  
+
   has_many :pieces
 
   belongs_to :white_player, :class_name => 'User'
   belongs_to :black_player, :class_name => 'User'
 
   validates :name, presence: true, length: { maximum: 140, minimum: 3 }
+
+
+  def black_player_joined?
+    black_player_id == current_user
+  end
 end
