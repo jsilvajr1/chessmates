e38624ea3ad5ea29b075a59a99b36f36a0843a7d
Arit Amana <>
2018-05-22 18:57:39 -0400

    Add captured boolean to pieces table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018 05 20 235845) do
+ActiveRecord::Schema.define(version: 20180520235845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "games", force: :cascade do |t|
     t.string   "game_name"
@@ -55,4 +53,5 @@ ActiveRecord::Schema.define(version: 2018 05 20 235845) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
