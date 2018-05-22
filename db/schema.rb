cd19d1ccabfb8bbb429f5e48eae2a4d6a54a68d4
Arit Amana <>
2018-05-22 19:00:00 -0400

    edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2018 05 20 235845) do
+ActiveRecord::Schema.define(version: 20180520235845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
