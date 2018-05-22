e77b52e185b8991e1403d676323282153b3c4372
Arit Amana <>
2018-05-22 16:47:15 -0400

    tests for show#pieces and update#pieces

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,22 +1,36 @@
 FactoryBot.define do
+
+  factory :piece do
+    location_x {rand(0..7)}
+    location_y {rand(0..7)}
+    white :true
+    association :game
+  end
+
   factory :king do
     
   end
+
   factory :queen do
     
   end
+  
   factory :rook do
     
   end
+  
   factory :bishop do
     
   end
+  
   factory :knight do
     
   end
+  
   factory :pawn do
     
   end
+  
   factory :white_player, class: User do
     sequence :username do |u|
       "username#{u}"
