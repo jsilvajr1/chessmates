c3629037bb22420a754a36bc3c1c439336c9785d
Bryan Jimenez <bryan@roguedevs.io>
2018-06-02 01:40:19 +0000

    Add rules and logic to pawn.rb

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,36 @@
 class Pawn < Piece
+
+  def valid_move?(dest_x, dest_y)
+    return false unless super
+    # Allow one place movement at a time
+    return false unless valid_fwd_move?(dest_x, dest_y)
+    return true
+  end
+
+  def capture(dest_x, dest_y)
+    if self.white
+      (dest_y - self.location_y) == 1 && d_obs?(dest_x, dest_y) && (dest_x - self.location_x).abs == 1
+    else
+      (dest_y - self.location_y) == -1 && d_obs?(dest_x, dest_y) && (dest_x - self.location_x).abs == 1 
+    end
+  end
+
+  def valid_fwd_move?(dest_x, dest_y)
+    if has_moved? && self.white
+      (dest_y - self.location_y) == 1 && dest_x == self.location_x
+    elsif has_moved? && !self.white
+      (dest_y - self.location_y) == -1 && dest_x == self.location_x
+    elsif !has_moved? && self.white
+      (dest_y - self.location_y) == 2 && dest_x == self.location_x
+    else
+      (dest_y - self.location_y) == -2 && dest_x == self.location_x
+    end
+  end
+
+  def has_moved?
+    return false if self.location_y == 1 && self.white
+    return false if self.location_y == 6 && !self.white
+    return true
+  end
+
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,4 +1,98 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
+  it "should allow white pawns to move horizontally 1 place at a time" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 1, location_y: 2, game_id: game.id, white:true)
+    piece1.save!
+    piece1.valid_move?(1,3)
+    expect(piece1.valid_move?(1,3)).to be true
+  end
+
+  it "should allow black pawns to move horizontally 1 place at a time" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 5, game_id: game.id, white:false)
+    piece1.save!
+    piece1.valid_move?(0,4)
+    expect(piece1.valid_move?(0,4)).to be true
+  end
+
+  it "shouldn't allow white pawns to move horizontally more than 1 place at a time" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 1, location_y: 1, game_id: game.id, white:true)
+    piece1.save!
+    piece1.valid_move?(2,2)
+    expect(piece1.valid_move?(2,2)).to be false
+  end
+
+  it "shouldn't allow black pawns to move horizontally more than 1 place at a time" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 6, game_id: game.id, white:false)
+    piece1.save!
+    piece1.valid_move?(0,3)
+    expect(piece1.valid_move?(0,3)).to be false
+  end
+
+  it "should check if it is white pawn inital movement" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 1, game_id: game.id, white:true)
+    piece1.save!
+    piece1.has_moved?
+    expect(piece1.has_moved?).to be false
+  end
+
+  it "should check if it is black pawn inital movement" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 6, game_id: game.id, white:false)
+    piece1.save!
+    piece1.has_moved?
+    expect(piece1.has_moved?).to be false
+  end
+
+  it "should check if it is white pawn inital movement" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 2, game_id: game.id, white:true)
+    piece1.save!
+    piece1.has_moved?
+    expect(piece1.has_moved?).to be true
+  end
+
+  it "should check if it is black pawn inital movement" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 5, game_id: game.id, white:false)
+    piece1.save!
+    piece1.has_moved?
+    expect(piece1.has_moved?).to be true
+  end
+
+  it "should allow 2 places movement if it's the first move" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 6, game_id: game.id, white:false)
+    piece1.save!
+    piece1.valid_move?(0,4)
+    expect(piece1.valid_move?(0,4)).to be true
+  end
+
+  it "should capture up one and one horizontal move to capture" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 1, game_id: game.id, white:true)
+    piece1.save!
+    piece2 = Pawn.create(location_x: 1, location_y: 2, game_id: game.id, white:false)
+    piece2.save!
+    piece1.capture(1,2)
+    expect(piece1.capture(1,2)).to be true
+  end
+
+  it "should capture up one and one horizontal move to capture" do
+    game = FactoryBot.create(:game)
+    piece1 = Pawn.create(location_x: 0, location_y: 6, game_id: game.id, white:false)
+    piece1.save!
+    piece2 = Pawn.create(location_x: 1, location_y: 5, game_id: game.id, white:true)
+    piece2.save!
+    piece1.capture(1,5)
+    expect(piece1.capture(1,5)).to be true
+  end
+
+
+
 end
