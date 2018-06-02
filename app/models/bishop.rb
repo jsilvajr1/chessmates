394f54fe4821df2daa66f3ade7e8e83af807a0fa
Jorge Silva <jorge.silva.jr1@gmail.com>
2018-06-02 18:37:21 +0000

    added valid moves to Bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,9 @@
 class Bishop < Piece
+  def legal_move?(x, y)
+    (x_position - x).abs == (y_position - y).abs
+  end
+
+  def obstructed_squares(x, y)
+    diagonal_obstruction_array(x, y)
+  end
 end
