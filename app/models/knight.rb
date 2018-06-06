63e4b45dfbc7cc99550d2146940275c77254b1a4
Jorge Silva <jorge.silva.jr1@gmail.com>
2018-06-06 01:27:10 +0000

    added valid moves for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,16 @@
 class Knight < Piece
-end
+
+  def valid_move?(x,y)
+    return false unless super(x,y)
+    x_diff = x_diff(x)
+    y_diff = y_diff(y)
+    if ((x_diff == 2) && (y_diff == 1))
+      return true
+    end
+
+    if ((y_diff == 2) && (x_diff == 1))
+      return true
+    end
+
+    return false
+  end
