bb2e50780378cc66f85fa9ad23021ca8741e91bf
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-06-06 20:38:27 +0000

    edit location of created rook piece in test so it doesn't interfere with game pieces originally created. vertical moves test wasn't working because of this issue.

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,22 +1,12 @@
 class Rook < Piece
 
   def valid_move?(x,y)
-    return false unless super(x,y)              #if move is invalid, it will return false immediately vs going thrrough code line by line - saves time. 
+    return false unless super(x,y)             
     current_loc_x = self.location_x             
     current_loc_y = self.location_y 
     
-    if current_loc_x == x && current_loc_y != y #checks if move is valid vertically
-      return v_obs?(x,y)                        # i initially had "return !v_obs?(x,y)" because it's supposed to return opp of v_obs. so if v_obs is false, returns true, 
-    end                                         # i.e. it is true the move is valid because it's not obstructed, but for some reason that kept causing my test to fail. i removed the ! and now it works but the logic doesn't make sense because return v_obs?(x,y) is basically "return false" because it's not obstructed and I want it to return true as in "it's true, it's valid"
-
-    if current_loc_y == y && current_loc_x != x #checks if move is valid horizontally
-      return !h_obs?(x,y)                       # returns opp of h_obs. so if h_obs is false, returns true, 
-    end                                         # i.e. it is true the move is valid
-
-    return false                                #if move is neither valid vertically or horizontally, then return false.
+    return !v_obs?(x,y) if current_loc_x == x && current_loc_y != y   
+    return !h_obs?(x,y)  if current_loc_y == y && current_loc_x != x
+                                                                    
   end
 end
-
-
-
-
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -4,36 +4,27 @@ RSpec.describe Rook, type: :model do
 
   it "should allow a rook to move vertically on board" do
     game = FactoryBot.create(:game)
-    piece1 = Rook.create(location_x: 1, location_y: 1, game_id: game.id)
-    expect(piece1.valid_move?(1,6)).to be true
+    rook = Rook.create(location_x: 4, location_y: 3, game_id: game.id)
+    expect(rook.valid_move?(4,5)).to be true
   end
 
   it "should allow a rook to move horizontally on board" do
     game = FactoryBot.create(:game)
-    piece1 = Rook.create(location_x: 3, location_y: 3, game_id: game.id)
-    expect(piece1.valid_move?(6,3)).to be true
+    rook = Rook.create(location_x: 3, location_y: 3, game_id: game.id)
+    expect(rook.valid_move?(6,3)).to be true
 
   end
 
   it "shouldn't allow moves outside the chessboard" do
     game = FactoryBot.create(:game)
-    piece1 = Rook.create(location_x: 0, location_y: 0, game_id: game.id)
-    expect(piece1.valid_move?(8,8)).to be false
-
+    rook = Rook.create(location_x: 5, location_y: 5, game_id: game.id)
+    expect(rook.valid_move?(8,8)).to be false
   end
 
+  it "shouldn't allow rook to move past an obstruction" do
+    game = FactoryBot.create(:game)
+    rook = Rook.create(location_x: 0, location_y: 3, game_id: game.id)
+    pawn = Pawn.create(location_x: 0, location_y: 5, game_id: game.id)
+    expect(rook.valid_move?(0,7)).to be false
+  end
 end
-
-=begin 
-  questions to review w/ roye
-  added tests for moving Hor & Ver. Check obstructed method specs - do they cover the following situations:
-    - if cur_loc is (1,3), dest is (6,3) but there's a team piece at (3,3). does obstructed method create an invalid path error
-      because can't jump pieces
-    - if cur_loc is (1,3), dest is (6,3) but there's an enemy at (3,3). does captured method know to capture the enemy piece
-      and update rook dest to (3,3) vs (6,3)? rook can't move forward, ie. can't capture then continue w initial dest plan
-    - if theres a team piece at destination. does obstructed method cite an error message "invalid path"?
-
-    - in the piece_spec.rb - why do we create pieces w/ some tests and find the pieces with others?
-    - move_to method in piece model accounts for destinations.
-    - recieving failure errow in test w/ line 27
-=end
