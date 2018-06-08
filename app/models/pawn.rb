863be35dd81699ffa5d7ca0154b70b9c9fe04658
Bryan Jimenez <bryan@roguedevs.io>
2018-06-08 01:34:49 +0000

    Run rubocop and made some changes

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -3,15 +3,15 @@ class Pawn < Piece
   def valid_move?(dest_x, dest_y)
     return false unless super
     # Allow one place movement at a time
-    return valid_fwd_move?(dest_x, dest_y)
+    valid_fwd_move?(dest_x, dest_y)
   end
 
   def diagonal_move?(dest_x, dest_y)
-    return (dest_x - self.location_x).abs == (dest_y - self.location_y).abs && (dest_x - self.location_x).abs == 1
+    (dest_x - self.location_x).abs == (dest_y - self.location_y).abs && (dest_x - self.location_x).abs == 1
   end
 
   def valid_fwd_move?(dest_x, dest_y)
-    return false unless dest_x == self.location_x || diagonal_move?(dest_x, dest_y) #&& capture_valid?(dest_x, dest_y)
+    return false unless dest_x == self.location_x || diagonal_move?(dest_x, dest_y) && capture_valid?(dest_x, dest_y)
     if has_moved? && self.white
       (dest_y - self.location_y) == 1
     elsif has_moved? && !self.white
@@ -31,7 +31,6 @@ class Pawn < Piece
   def has_moved?
     return false if self.location_y == 1 && self.white
     return false if self.location_y == 6 && !self.white
-    return true
   end
 
 end
