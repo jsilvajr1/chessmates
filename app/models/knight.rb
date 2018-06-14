b84a0b162ace38bf665e965bd2e12d5d377f5611
Jorge Silva <jorge.silva.jr1@gmail.com>
2018-06-14 17:24:15 +0000

    refactored code again

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,12 +1,14 @@
-def valid_move?(x, y)
-		return false if no_move?(x,y)
+class Knight < Piece
 
-		if  ((x - x_coordinate).abs == 2 && (y - y_coordinate).abs == 1) ||
-			((x - x_coordinate).abs == 1 && (y - y_coordinate).abs == 2 ) &&
-			((x != x_coordinate) && (y != y_coordinate))
-			return true
-		else
-			return false
-		end
-	end
+def valid_move?(x, y)
+#return false if
+
+ if ((x - location_x).abs == 2 && (y - location_y).abs == 1) ||
+   ((x - location_x).abs == 1 && (y - location_y).abs == 2 ) &&
+   ((x != location_x) && (y != location_y))
+   return true
+ else
+   return false
+   end
+ end
 end
